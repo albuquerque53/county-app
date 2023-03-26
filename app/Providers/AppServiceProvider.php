19a9b0cc17e727bf70ffd74d9abf1f71d35894a3
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2023-03-26 14:07:07 -0300

    chore: defining the dependencies of services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->bind(AbstractCountyService::class, function (): AbstractCountyService {
+            $service = env('COUNTY_SERVICE');
 
+            return CountyServiceFactory::createCountyService($service);
+        });
+
+        $this->app->bind(BrasilApiCountyService::class, function (): BrasilApiCountyService {
+            $client = new Client(
+                [
+                    'base_uri' => BrasilApiCountyService::BASE_URI,
+                ]
+            );
+
+            return new BrasilApiCountyService($client);
+        });
+
+        $this->app->bind(IbgeCountyService::class, function (): IbgeCountyService {
+            $client = new Client(
+                [
+                    'base_uri' => IbgeCountyService::BASE_URI,
+                ]
+            );
+
+            return new IbgeCountyService($client);
+        });
     }
 
     /**
