b5255909241aa4d410160dc00f27fce57de3aa47
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2023-03-26 14:06:40 -0300

    feat: factory to services

---
+++ app/Services/CountyServiceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Abstraction\AbstractCountyService;
+use Illuminate\Support\Facades\App;
+
+class CountyServiceFactory
+{
+    public const VALID_SERVICES = [
+        'brasil_api' => BrasilApiCountyService::class,
+        'ibge_api' => IbgeCountyService::class,
+    ];
+
+    public static function createCountyService(string $serviceName): AbstractCountyService
+    {
+        $service = self::VALID_SERVICES[$serviceName] ?? null;
+
+        if (is_null($service)) {
+            // TODO: remove this exception
+            throw new \Exception('The service provided is not valid');
+        }
+
+        return App::get($service);
+    }
+}
