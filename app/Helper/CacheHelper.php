bd4ebcc6424aa01b0ba44798b4fea841df88cdfa
Gabriel Albuquerque <gabrielalbuquerque-dev@hotmail.com>
2023-04-07 16:25:45 -0300

    chore: adding comments to CaceHelper methods

--- app/Helper/CacheHelper.php
+++ app/Helper/CacheHelper.php
@@ -8,6 +8,13 @@ class CacheHelper
 {
     private const CACHE_TEMPLATE = 'C%s_P%d_S%d';
 
+    /**
+     * Verify if there are saved cache for informed $countyCode, $pageNumber and $pageSize
+     * @param string $countyCode
+     * @param int $pageNumber
+     * @param int $pageSize
+     * @return array|null
+     */
     public static function verifyCacheForCountyCode(string $countyCode, int $pageNumber, int $pageSize): ?array
     {
         $cacheName = self::buildCacheName($countyCode, $pageNumber, $pageSize);
@@ -20,12 +27,27 @@ class CacheHelper
         return $cacheResult;
     }
 
+    /**
+     * Save the $data in cache for the informed $countyCode, $pageNumber and $pageSize
+     * @param string $countyCode
+     * @param int $pageNumber
+     * @param int $pageSize
+     * @param array $data
+     * @return void
+     */
     public static function saveCache(string $countyCode, int $pageNumber, int $pageSize, array $data): void
     {
         $cacheName = self::buildCacheName($countyCode, $pageNumber, $pageSize);
         Cache::put($cacheName, $data, now()->addMinutes(10));
     }
 
+    /**
+     * Returns the cache name that follows the format: "C<county-code>_P<page-number>_S<page-size>"
+     * @param string $countyCode
+     * @param int $pageNumber
+     * @param int $pageSize
+     * @return string
+     */
     private static function buildCacheName(string $countyCode, int $pageNumber, int $pageSize): string
     {
         return sprintf(self::CACHE_TEMPLATE, $countyCode, $pageNumber, $pageSize);
