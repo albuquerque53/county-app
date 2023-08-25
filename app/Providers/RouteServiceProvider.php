763a8804c55c2f2a00ca9ccc9ee638309a01dae2
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-25 12:30:24 -0300

    feat: to add web routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
+                ->prefix('api')
                 ->middleware(ExceptionHandler::class)
                 ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
 
