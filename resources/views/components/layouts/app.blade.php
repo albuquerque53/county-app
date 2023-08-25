6ad0261df9cb6d2771d1676402e4324988ea5b54
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-25 13:49:36 -0300

    feat: to install and configure tailwind css

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @vite('resources/css/app.css')
     </head>
     <body>
         {{ $slot }}
