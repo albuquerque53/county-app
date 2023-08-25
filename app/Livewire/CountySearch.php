33d8bd1af0f51dfb8ad1346af1756f8d9548776f
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-25 13:50:37 -0300

    chore: to define a title for county search page

--- app/Livewire/CountySearch.php
+++ app/Livewire/CountySearch.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Enums\CountyCodeEnum;
 use App\Repository\CountyRepositoryInterface;
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class CountySearch extends Component
@@ -12,6 +13,7 @@ class CountySearch extends Component
     public ?string $result = null;
     public ?string $countyCode = null;
 
+    #[Title('County search')]
     public function render(): View
     {
         return view('livewire.county-search');
