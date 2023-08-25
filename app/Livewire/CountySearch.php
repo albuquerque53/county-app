1261c833125c190c947aa965ad3738b3dfe36eff
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-25 15:33:58 -0300

    chore: to improve code from county component

--- app/Livewire/CountySearch.php
+++ app/Livewire/CountySearch.php
@@ -5,12 +5,13 @@ namespace App\Livewire;
 use App\Enums\CountyCodeEnum;
 use App\Repository\CountyRepositoryInterface;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class CountySearch extends Component
 {
-    public ?string $result = null;
+    public ?Collection $result = null;
     public ?string $countyCode = null;
 
     #[Title('County search')]
@@ -21,13 +22,18 @@ class CountySearch extends Component
 
     public function searchForCounty(): void
     {
-        /** @var CountyRepository $countyRepository */
+        if (blank($this->countyCode)) {
+            return;
+        }
+
+        /** @var CountyRepositoryInterface $countyRepository */
         $countyRepository = app(CountyRepositoryInterface::class);
 
-        $this->result = $countyRepository->findByCountyCode(
-            countyCode: CountyCodeEnum::from(strtoupper($this->countyCode)),
-            pageNumber: 1,
-            pageSize: 100,
-        );
+        $this->result = $countyRepository
+            ->findByCountyCode(
+                countyCode: CountyCodeEnum::from(strtoupper($this->countyCode)),
+                pageNumber: 1,
+                pageSize: 100,
+            );
     }
 }
